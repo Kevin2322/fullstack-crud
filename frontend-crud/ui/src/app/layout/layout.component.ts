1aeaeb6f1fe5d05e6ac199bc3fcd9a0fb2ffb4bf
Kevin2322 <vankawalakevin153@gmail.com>
2024-02-10 17:09:37 +0530

    added

--- frontend-crud/ui/src/app/layout/layout.component.ts
+++ frontend-crud/ui/src/app/layout/layout.component.ts
@@ -4,6 +4,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+class Employee{
+  eid?:number;
+  employeename?:string;
+  salary?:string;
+  designation?:string;
+}
 
 @Component({
   selector: 'app-layout',
@@ -15,24 +21,69 @@ import { RouterOutlet } from '@angular/router';
 export class LayoutComponent implements OnInit {
 
   public employees : any;
+  eid:number = 0;
+  employeename:string = '';
+  designation:string = '';
+  salary:string = '';
+  isEdit:boolean = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-      this.employeeList()
+      this.getEmployeeList()
   }
 
-   public employeeList()  {
+   public getEmployeeList()  {
       return this.http.get('http://localhost:7000/showemployee').subscribe( (data) =>{
       console.log(data);
       this.employees = data;
     });
-    // console.log(api);
+   }
 
+  OnSave(){
+    const tempModal = new Employee();
+    tempModal.employeename = this.employeename;
+    tempModal.designation = this.designation;
+    tempModal.salary = this.salary;
+
+    return this.http.post('http://localhost:7000/addemployee', tempModal).subscribe((res:any) => {
+      this.getEmployeeList();
+      this.clearItems();
+    })
   }
 
-  trackByFn(index: any,employee: any): any {
-    return employee.eid;
+  OnClickEdit(row:any){
+    this.isEdit = true;
+    this.eid = row.eid;
+    this.employeename = row.employeename;
+    this.designation = row.designation;
+    this.salary = row.salary;
+  }
+
+  OnEdit(){
+    const tempModal = new Employee();
+    tempModal.eid = this.eid;
+    tempModal.employeename = this.employeename;
+    tempModal.designation = this.designation;
+    tempModal.salary = this.salary;
+
+    return this.http.put('http://localhost:7000/updateemployee/'+this.eid, tempModal).subscribe((res:any) => {
+      this.getEmployeeList();
+      this.clearItems();
+    })
+  }
+
+  OnDelete(id:number){
+    return this.http.delete('http://localhost:7000/deleteemployee/'+id).subscribe((res:any) => {
+      this.getEmployeeList();
+    })
+  }
+
+  clearItems(){
+    this.employeename='';
+    this.designation='';
+    this.salary = '';
+    this.isEdit=false;
   }
 
 }
